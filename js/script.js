6bc33fd6f923edfafe5f61f0406a5d5bc38264a7
MSDJHD <msdjhd@163.com>
2024-07-29 16:48:29 +0800

    Site updated: 2024-07-29 16:48:28

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-function pjax_reload() {document.getElementsByClassName("post-copyright")[0].innerHTML = '<div class="post-copyright-content"><img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></div><div class="post-copyright-text"><p>采用<a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>进行许可</p></div>';}
-document.addEventListener("pjax:complete", function() {pjax_reload();});
+function pjax_reload() {
+    // 重置文章版权信息
+    document.getElementsByClassName("post-copyright")[0].innerHTML = '<div class="post-copyright-content"><img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></div><div class="post-copyright-text"><p>采用<a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>进行许可</p></div>';
+}
+document.addEventListener("pjax:complete", function () { pjax_reload(); });
 document = pjax_reload();
